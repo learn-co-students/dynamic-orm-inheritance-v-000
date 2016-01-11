33b8f1f1f41419b024cdccb59cd93fbd66164022
lizdenhup <lizdenhup@gmail.com>
2016-01-11 15:13:53 -0600

    finished ORM inheritance read-along

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,7 @@
 require_relative "./interactive_record.rb"
 
 class Song < InteractiveRecord
+  #this is a common design pattern.
   
   self.column_names.each do |col_name|
     attr_accessor col_name.to_sym
