1008e10adef4c04c9b71de21b13a53b65e6fbc4c
Rawradin <edgar.rawr@gmail.com>
2016-01-25 19:42:39 -0800

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,7 @@ require_relative "./interactive_record.rb"
 
 class Song < InteractiveRecord
   
-  self.column_names.each do |col_name|
+  self.column_names.each do |col_name| #doesnt inherit InteractiveRecord's self.column_names.each since its not a method
     attr_accessor col_name.to_sym
   end
 
