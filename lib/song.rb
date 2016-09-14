a87cfa1331d81f35056a4ebf9db449272a472579
Lani1321 <lani.berry13@gmail.com>
2016-09-14 10:18:57 -0400

    add comments

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,6 @@
 require_relative "./interactive_record.rb"
 
+# Song class inherits from InteractiveRecord and uses all of it's methods
 class Song < InteractiveRecord
 
   self.column_names.each do |col_name|
