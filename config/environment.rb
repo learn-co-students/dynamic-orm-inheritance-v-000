1577fa909acd747268afb294fbef7031f4557978
Jonathan Foster <redcabinny@gmail.com>
2017-01-28 15:39:19 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,8 @@ require 'sqlite3'
 
 DB = {:conn => SQLite3::Database.new("db/songs.db")}
 DB[:conn].execute("DROP TABLE IF EXISTS songs")
+DB[:conn].execute("DROP TABLE IF EXISTS authors")
+
 sql = <<-SQL
   CREATE TABLE IF NOT EXISTS songs (
   id INTEGER PRIMARY KEY,
@@ -11,5 +13,15 @@ sql = <<-SQL
   )
 SQL
 
+sql2 = <<-SQL
+  CREATE TABLE IF NOT EXISTS authors (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    nom_de_plumme TEXT,
+    age INTEGER
+  )
+  SQL
+
 DB[:conn].execute(sql)
+DB[:conn].execute(sql2)
 DB[:conn].results_as_hash = true
