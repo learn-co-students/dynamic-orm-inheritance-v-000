93063a12c6b256958a29747d6e748b4543703cab
Daniel Lempesis <danlrcn@hotmail.com>
2017-03-24 00:09:08 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,10 +1,9 @@
 require_relative "../config/environment.rb"
-require 'active_support/inflector'
 
 class InteractiveRecord
 
   def self.table_name
-    self.to_s.downcase.pluralize
+    "#{self.to_s.downcase}s"
   end
 
   def self.column_names
