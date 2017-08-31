dea3cec98bf1723c5e61e7edc64c30a2dd3713eb
johnfelixespinosa <johnfelix.espinosa@icloud.com>
2017-08-31 15:57:49 -0400

    initial commit

--- config/environment.rb
+++ config/environment.rb
@@ -1,13 +1,13 @@
 require 'sqlite3'
 
 
-DB = {:conn => SQLite3::Database.new("db/songs.db")}
-DB[:conn].execute("DROP TABLE IF EXISTS songs")
+DB = {:conn => SQLite3::Database.new("db/groups.db")}
+DB[:conn].execute("DROP TABLE IF EXISTS groups")
 sql = <<-SQL
-  CREATE TABLE IF NOT EXISTS songs (
+  CREATE TABLE IF NOT EXISTS groups (
   id INTEGER PRIMARY KEY,
   name TEXT,
-  album TEXT
+  subs TEXT
   )
 SQL
 
