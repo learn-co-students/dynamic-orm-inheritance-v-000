d19634d0394cd50549ce56aa0be7930b41337bc6
Robert Hughes <roberts.ginger.email@gmail.com>
2018-02-25 06:20:54 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -13,11 +13,7 @@ class InteractiveRecord
     sql = "pragma table_info('#{table_name}')"
 
     table_info = DB[:conn].execute(sql)
-    column_names = []
-    table_info.each do |row|
-      column_names << row["name"]
-    end
-    column_names.compact
+    table_info.map { |row| row["name"] }.compact
   end
 
   def initialize(options={})
@@ -27,9 +23,11 @@ class InteractiveRecord
   end
 
   def save
-    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert})
+      VALUES (#{values_for_insert})"
     DB[:conn].execute(sql)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+    @id = DB[:conn].execute("SELECT last_insert_rowid()
+      FROM #{table_name_for_insert}")[0][0]
   end
 
   def table_name_for_insert
@@ -37,11 +35,9 @@ class InteractiveRecord
   end
 
   def values_for_insert
-    values = []
-    self.class.column_names.each do |col_name|
-      values << "'#{send(col_name)}'" unless send(col_name).nil?
-    end
-    values.join(", ")
+    self.class.column_names.map do |col_name|
+      "'#{send(col_name)}'" unless send(col_name).nil?
+    end.compact.join(", ")
   end
 
   def col_names_for_insert
