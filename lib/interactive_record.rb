064c80b9ed61bf569e3271c81c876f87cd0e5ae0
David Markel <davidgmarkel@gmail.com>
2018-04-30 22:37:53 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,6 +1,5 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
-
 class InteractiveRecord
 
   def self.table_name
@@ -9,7 +8,6 @@ class InteractiveRecord
 
   def self.column_names
     DB[:conn].results_as_hash = true
-
     sql = "pragma table_info('#{table_name}')"
 
     table_info = DB[:conn].execute(sql)
