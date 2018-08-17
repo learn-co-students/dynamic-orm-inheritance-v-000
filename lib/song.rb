19fcf713288da56cf411f72d7fea1d5c3fb99892
Madeline Stark <madeline.stark1@gmail.com>
2018-08-17 21:48:16 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,7 @@ require_relative "./interactive_record.rb"
 class Song < InteractiveRecord
 
   self.column_names.each do |col_name|
-    attr_accessor col_name.to_sym
+    attr_accessor col_name.to_sym #need to create attr_accessors specific to class but everything else can be inherited
   end
 
 end
